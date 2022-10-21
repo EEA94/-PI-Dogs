a448f1f2a97e3a8ca03691f9a5e6692d59b3a024
EEA94 <14elias06andrada94@gmail.com>
2022-10-21 00:36:24 -0300

    command start added

--- api/src/config.js
+++ api/src/config.js
@@ -1,8 +1,8 @@
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_HOST = process.env.DB_HOST
-const DB_NAME = process.env.DB_NAME
-const PORT = process.env.PORT
+const DB_USER = process.env.DB_USER || 'postgres'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Elias1406'
+const DB_HOST = process.env.DB_HOST || 'localhost:5432'
+const DB_NAME = process.env.DB_NAME || 'dogs'
+const PORT = process.env.PORT || 3001
 
 const API_KEY = process.env.API_KEY
 
