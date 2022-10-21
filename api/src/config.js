590ee61d6ddb08583ea76ae8aeece4444ff5c3a5
EEA94 <14elias06andrada94@gmail.com>
2022-10-20 23:49:25 -0300

    configured to deploy

---
+++ api/src/config.js
@@ -0,0 +1,16 @@
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_NAME = process.env.DB_NAME
+const PORT = process.env.PORT
+
+const API_KEY = process.env.API_KEY
+
+module.exports = {
+DB_USER,
+DB_PASSWORD,
+DB_HOST,
+DB_NAME,
+PORT,
+API_KEY
+}
