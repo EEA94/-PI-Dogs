73c6664d5529cfd4b8b01ad8fba98f2b0686948e
EEA94 <14elias06andrada94@gmail.com>
2022-10-21 21:14:37 -0300

    revert commit

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from "react";
+import React,{ useState } from "react";
 import { useDispatch, useSelector} from "react-redux";
 import { getDogs, filterDb, filterApi, filterTemp, orderName, orderWeight, orderHeight} from "../redux/actions";
 import styles from "../styles/Filters.module.css"
