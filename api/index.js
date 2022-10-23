e1cc95881b07308b124536e83eda4608d9a7846f
EEA94 <14elias06andrada94@gmail.com>
2022-10-23 00:20:03 -0300

    try configuration for deploy

--- api/index.js
+++ api/index.js
@@ -2,8 +2,16 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {DB_PORT} = require('./src/config.js')
 
+
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: false }).then(async () => {
+  try {
+    await conn.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
   server.listen(DB_PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
